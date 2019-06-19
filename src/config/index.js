b1ecc53b913ffe49957bc99790aec04707c60384
jianjiayi <m13121378101@163.com>
2019-06-19 16:10:22 +0800

    first commit centos

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 //ajax配置
-// export const BASE_URL = "http://39.97.108.64:3000";
-export const BASE_URL = "http://192.168.1.103:3000";
+export const BASE_URL = "http://59.110.136.67";
+// export const BASE_URL = "http://192.168.1.103:3000";
 // export const BASE_URL = "http://192.168.43.201:3000";
 export const HTTP_TIMEOUT = "10000";
 
